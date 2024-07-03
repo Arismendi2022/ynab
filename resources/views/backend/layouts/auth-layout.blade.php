1761cd42c1d8e1d2a8f930cd6cbe144a3490a1d0
arismendi2022 <arismendi.guiza@gmail.com>
2024-07-03 10:46:24 -0500

    feat: Final update login

--- resources/views/backend/layouts/auth-layout.blade.php
+++ resources/views/backend/layouts/auth-layout.blade.php
@@ -6,12 +6,13 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="description"
-	content="Personal home budget software built with Four Simple Rules to help you quickly gain control of your money, get out of debt, and reach your financial goals!">
+		content="Personal home budget software built with Four Simple Rules to help you quickly gain control of your money, get out of debt, and reach your financial goals!">
 	<title>@yield('pageTitle')</title>
 	
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Google+Sans" rel="stylesheet">
 	<link rel="shortcut icon" href="/backend/assets/brand/ynab-tree-logo.svg">
 	<link href="/backend/assets/dist/css/auth.css" rel="stylesheet">
 	
@@ -21,13 +22,22 @@
 
 </head>
 
-<body class="" data-page="authentications#new" data-ynab-device="web">
+<body data-page="authentications#new" data-ynab-device="web" class="theme-dark body-background--dark-shapes">
 <div class="page-wrapper">
-	<header class="page-header">
-		<a title="YNAB" class="ynab-primary-logo launch_app_button" href="/"><img alt=""
-			src="/backend/assets/brand/ynab-primary-logo.svg">
-		</a>
+	<header class="authentications-page-header">
+		<div class="authentications-page-header__inner">
+			<a title="Back to ynab.com" class="authentications-page-header__logo" href="https://www.ynab.com"><img alt=""
+					src="/backend/assets/brand/ynab-logo-buttermilk.svg"></a>
+			<a class="authentications-page-header__back-button" href="https://www.ynab.com">
+				<svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" fill="#FEF9ED" viewBox="0 0 18 18" width="0.75em" height="0.75em">
+					<path fill="inherit"
+						d="M.8 9c0-.3 0-.5.2-.7l5.8-5.8.3-.2h.3c.2 0 .4 0 .6.2l.2.5a.8.8 0 0 1-.2.6l-2 2-3.5 3.1-.2-.4 2.9-.2h11.1l.6.2.2.6-.2.6-.6.2H5.2l-2.9-.2.2-.4L6 12.3l2 2 .1.2.1.3c0 .2 0 .4-.2.6l-.6.2c-.2 0-.4-.1-.5-.3L1 9.5a.8.8 0 0 1-.3-.6Z"></path>
+				</svg>
+				Back to ynab.com
+			</a>
+		</div>
 	</header>
+	<!---->
 	<main class="page-main" role="main">
 		@yield('content')
 	</main>
@@ -36,13 +46,15 @@
 <!---->
 
 {{-- footer --}}
-<footer>
-	<a rel="noopener noreferrer" target="_blank" href="#" onclick="return false;">Terms of Service</a>
-	<a rel="noopener noreferrer" target="_blank" href="#" onclick="return false;">Privacy Policy</a>
-	<a rel="noopener noreferrer" target="_blank" href="#" onclick="return false;">California Privacy Policy</a>
-	<button class="your-privacy-choices js-open-preference-center">Your Privacy Choices</button>
-	<div class="copyright">© Copyright {{ date('Y') }} YNAB LLC. All rights reserved.</div>
-</footer>
+<div class="application__footer">
+	<footer class="page-footer">
+		<a rel="noopener noreferrer" target="_blank" href="#" onclick="return false;">Terms of Service</a>
+		<a rel="noopener noreferrer" target="_blank" href="#" onclick="return false;">Privacy Policy</a>
+		<a rel="noopener noreferrer" target="_blank" href="#" onclick="return false;">California Privacy Policy</a>
+		<button class="page-footer__privacy-choices js-open-preference-center">Your Privacy Choices</button>
+		<div class="copyright">© Copyright {{ date('Y') }}  YNAB LLC. All rights reserved.</div>
+	</footer>
+</div>
 
 <script src="/frontend/assets/dist/js/jquery-3.7.1.min.js"></script>
 
--- resources/views/backend/pages/admin/auth/sign_in.blade.php
+++ resources/views/backend/pages/admin/auth/sign_in.blade.php
@@ -1,86 +1,113 @@
 @extends('backend.layouts.auth-layout')
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Admin Login')
 @section('content')
-	<div id="login-screen" data-controller="login" data-login-otp-form-outlet="#otp-form">
-		<div data-login-target="loginContainer">
-			<section class="aside-content">
-				<aside>
-          <span class="doodle">
-            <img src="/backend/assets/brand/doodle-logo.svg">
-          </span>
-					<p>
-						Create the life you want with YNAB, a money management method that will put you on the path to less money stress.
-					</p>
-				</aside>
-				<div class="authentications-container">
-					<div class="authentications-container-item">
-						<div data-login-target="loginHeader">
-							<h2>Log In</h2>
-						</div>
-						<div data-login-target="loginSubheader">
-							<p>New to YNAB? <a data-action="login#trackClickedSignUp" href="{{ route('users.sign_up') }}">Sign up today</a>.</p>
-						</div>
-						<form id="login-form" class="form login-form" action="{{ route('users.login_handler') }}" method="POST" accept-charset="UTF-8" novalidate>
-							@csrf
-							<div data-login-target="identityContainer">
-								<p class="js-form-email">
-									<label for="request_data_email" class="u-sr-only">Email:</label>
-									<input class="required" autofocus="autofocus" spellcheck="false" placeholder="Email address" data-login-target="emailInput" type="email"
-									name="email" id="request_data_email" value="{{ old('email') }}">
-									@error('email')
-									<label class="error" for="request_data_email">{{ $message }}</label>
-									@enderror
-								</p>
-								<p class="js-form-password">
-									<label class="u-sr-only" for="request_data_password">Password:</label>
-									<span class="password-toggle">
-										<input class="required" placeholder="Password" data-login-target="passwordInput" autocapitalize="none" autocomplete="off"
-										type="password" name="password" id="request_data_password" value="{{ old('password') }}">
-										<label><input type="checkbox" id="togglePassword" data-password-toggle="">Show</label></span>
-									@error('password')
-									<label class="error" for="request_data_password">{{ $message }}</label>
-									@enderror
-								</p>
-								<p class="remember-me-and-forgot-password">
-									<label for="request_data_remember_me">
-										<input data-login-target="rememberMeInput" type="checkbox" value="1" name="request_data[remember_me]" id="request_data_remember_me">
-										Keep me logged in
-									</label> <a href="{{ route('users.forgot-password') }}">Forgot password?</a>
-								</p>
-								<p>
-								</p>
-								<div>
-									<button name="login" type="submit" id="login" class="button button-primary" data-disable-with="Logging In...">Log In</button>
-								</div>
-							</div>
-							<div data-login-target="ssoButtons">
-								<div class="sso-buttons">
-									<hr class="u-hr-text" data-content="or">
-									<p class="u-sr-only">Or sign up with your Apple or Google account</p>
-									<a class="button-brand button-brand-apple" data-label="Continue with Apple" data-trigger-action="false" data-login-target="appleButton"
-									href="#" onclick="return false;">
-										<span class="button-brand-logo"><img class="provider-logo"
-											src="/backend/assets/brand/apple-logo.svg"></span><span
-										class="button-brand-name">Continue with Apple
-										</span>
-									</a>
-									<p class="apple-error"></p>
-									<div class="button-brand button-brand-google" data-width="288" data-login-target="googleButton">
-										<div class="button-brand-google-inner js-disabled"><span class="button-brand-logo"><img class="provider-logo"
-												src="/backend/assets/brand/google-logo.svg"/></span><span
-											class="button-brand-name">Continuar con Google</span></div>
-										<div id="gsi_312426_985296-overlay" class="L5Fo6c-bF1uUb" tabindex="0"></div>
-									</div>
-									<p class="google-error"></p>
-								</div>
-							</div>
-							<!---->
-						</form>
+	<section id="login-screen" class="authentications-section" data-controller="login" data-login-otp-form-outlet="#otp-form">
+		<div class="authentications-section__inner">
+			<aside class="authentications-aside">
+				<div class="authentications-aside__image authentications-aside__image--default"><img
+						src="/backend/assets/brand/ynab-tree-logo.svg"></div>
+				<h2>Transforma el dinero.</h2>
+				<p>
+					YNAB ha ayudado a millones de personas a aprender a gastar, ahorrar y vivir con alegría con un conjunto simple de hábitos que cambian la vida.
+				</p>
+			</aside>
+			
+			<div class="authentications-panel">
+				<div class="authentications-panel__content">
+					<div data-login-target="loginHeader">
+						<h2>Log In</h2>
+					</div>
+					<div data-login-target="backupCodeHeader" hidden="">
+						<h2>Log In With An Emergency Backup Code</h2>
+						<p>As a last resort, log in to YNAB with an emergency backup code. This code was given to you when you first enabled two-step verification.</p>
+					</div>
+					<div data-login-target="loginSubheader">
+						<p>
+							New to YNAB?
+							<a data-action="login#trackClickedSignUp" href="{{ route('users.sign_up') }}">Sign up today.</a>
+						</p>
 					</div>
 				</div>
-			</section>
+				<form id="login-form" class="authentications-panel__form" data-login-target="form" action="{{ route('users.login_handler') }}" accept-charset="UTF-8" method="POST"
+					novalidate="novalidate">
+					@csrf
+					<div data-login-target="identityContainer">
+						<p class="authentications-panel__input-group js-form-email">
+							<label class="u-sr-only" for="request_data_email">Email:</label>
+							<input class="authentications-panel__email-field required" autofocus="autofocus" spellcheck="false" placeholder="Email address" data-login-target="emailInput"
+								type="email" name="email" id="request_data_email" value="{{ old('email') }}">
+							@error('email')
+							<label class="error" for="request_data_email">{{ $message }}</label>
+							@enderror
+						</p>
+						<p class="authentications-panel__input-group js-form-password">
+							<label class="u-sr-only" for="request_data_password">Password:</label>
+							<span class="password-toggle"><input class="authentications-panel__password-field required" placeholder="Password" data-login-target="passwordInput"
+									autocapitalize="none" autocomplete="off" type="password" name="password" id="request_data_password" value="{{ old('password') }}">
+								<label><input type="checkbox" id="togglePassword" data-password-toggle="" data-gtm-form-interact-field-id="0">Show</label>
+							</span>
+							@error('password')
+							<label class="error" for="request_data_password">{{ $message }}</label>
+							@enderror
+						</p>
+						<p class="authentications-panel__form-options">
+							<label for="request_data_remember_me">
+								<input data-login-target="rememberMeInput" type="checkbox" value="1" name="request_data[remember_me]" id="request_data_remember_me">
+								Keep me logged in
+							</label> <a href="{{ route('users.forgot-password') }}">Forgot password?</a>
+						</p>
+						<p>
+							<button name="login" type="submit" id="login-button" class="authentications-panel__form-button button button-primary" data-disable-with="Logging In...">Log In
+							</button>
+						</p>
+					</div>
+					<div class="authentications-panel__otp-form">
+						<div id="otp-form" data-controller="otp-form" data-otp-form-target="otpContainer" data-otp-form-using-backup-code-value="false" hidden="">
+							<div>
+								<p class="js-form-otp">
+									<label data-otp-form-target="label" for="request_data_otp">Enter the 6-digit code from your authenticator app:</label>
+									<input placeholder="6-digit code" maxlength="6" class="required" autocomplete="off" autofocus="autofocus" data-otp-form-target="input" size="6" type="text"
+										name="request_data[otp]" id="request_data_otp">
+									<label class="error" for="request_data_otp"></label>
+								</p>
+							</div>
+							<br>
+							<button name="button" type="submit" class="button button-primary" data-disable-with="Verifying...">Verify</button>
+							<div>
+								<p data-otp-form-target="showBackupLink">
+									<a href="#" data-action="click->otp-form#showBackup">I'm having trouble</a>
+								</p>
+								<p data-otp-form-target="troubleshootLink" hidden="">
+									<a rel="noopener noreferrer" target="_blank"
+										href="#">I don't have my backup code</a>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div data-login-target="ssoButtons">
+						<div class="authentications-sso-buttons">
+							<hr class="u-hr-text authentications-sso-buttons__separator" data-content="or"/>
+							<div class="u-sr-only">Or sign up with your Apple or Google account</div>
+							<div class="authentications-sso-buttons__button">
+								<a class="sso-button sso-button--apple js-disabled" data-label="Continue with Apple" data-trigger-action="false" data-login-target="appleButton"
+									href="#"><span class="sso-button__logo"><img class="sso-provider-logo"
+											src="/backend/assets/brand/apple-logo.svg"/></span><span class="sso-button__label">Continue with Apple</span></a>
+								<p class="authentications-sso-button__error authentications-sso-button__error--apple"></p>
+							</div>
+							<div class="authentications-sso-buttons__button">
+								<div class="sso-button sso-button--google" data-login-target="googleButton">
+									<div class="sso-button__inner js-disabled" aria-labelledby="button-label"><span class="sso-button__logo"><img class="sso-provider-logo"
+												src="/backend/assets/brand/google-logo.svg"/></span><span
+											class="sso-button__label1">Continuar con Google</span></div>
+								</div>
+								<p class="authentications-sso-button__error authentications-sso-button__error--google"></p>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
 		</div>
-	</div>
+	</section>
 @endsection
 @push('scripts')
 	<script>
@@ -115,6 +142,18 @@
 				});
 			}
 		});
+
+		//Evitar que la pagina se recarge
+		/*const formLogin = document.querySelector("#login-form");
+
+		formLogin.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const request = new XMLHttpRequest();
+
+			//console.log('Form has been submitted:');
+
+		});*/
 	
 	</script>
 @endpush
