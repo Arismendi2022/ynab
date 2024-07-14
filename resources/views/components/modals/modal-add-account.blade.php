56e94d1c19e083d0ff185ec3879b22f485e2910b
arismendi2022 <arismendi.guiza@gmail.com>
2024-07-14 11:30:52 -0500

    feat: final final structure add account

--- resources/views/components/modals/modal-add-account.blade.php
+++ resources/views/components/modals/modal-add-account.blade.php
@@ -2,7 +2,7 @@
 <div id="ember145" class="ynab-u modal-generic self-centered account-widget-modal ">
 	<div class="modal" role="dialog" aria-modal="true">
 		<div class="account-widget" tabindex="0">
-			<div class="account-widget-step account-widget-select-linked-unlinked account-widget-step-with-help-box">
+			{{-- <div class="account-widget-step account-widget-select-linked-unlinked account-widget-step-with-help-box">
 				<div class="account-widget-header">
 					<div class="hidden-header-button"></div>
 					<div class="account-widget-header-title">
@@ -90,6 +90,183 @@
 						</p>
 					</div>
 				</div>
+			</div> --}}
+			<!---->
+			{{-- <div class="account-widget-step account-widget-add-unlinked-account account-widget-step-with-help-box">
+				<div class="account-widget-header">
+					<button aria-label="Back" title="Back" type="button">
+						<svg class="ynab-new-icon icon-back" width="16" height="16">
+							<!---->
+							<use href="#icon_sprite_chevron_left">
+								<symbol xmlns="http://www.w3.org/2000/svg" id="icon_sprite_chevron_left" fill="none" viewBox="0 0 24 24">
+									<path fill="currentColor" fill-rule="evenodd"
+										d="M18.7 23.6a2 2 0 0 1-2.1 0L5.3 13a1.4 1.4 0 0 1 0-2L16.6.4a2 2 0 0 1 2 0c.7.6.7 1.5 0 2L8.6 12l10.2 9.6c.6.5.6 1.4 0 2" clip-rule="evenodd"></path>
+								</symbol>
+							</use>
+						</svg>
+					</button>
+					<div class="account-widget-header-title">
+						<h1>Add Unlinked Account</h1>
+						<!---->
+					</div>
+					<button aria-label="Close" title="Close" type="button">
+						<svg class="ynab-new-icon icon-close" width="16" height="16">
+							<!---->
+							<use href="#icon_sprite_close">
+								<symbol xmlns="http://www.w3.org/2000/svg" id="icon_sprite_close" fill="none" viewBox="0 0 24 24">
+									<path fill="currentColor" fill-rule="evenodd"
+										d="M22.5 22.5a1.4 1.4 0 0 1-2 0L12 13.9l-8.6 8.6a1.4 1.4 0 0 1-1.9-2l8.6-8.5-8.6-8.5a1.4 1.4 0 0 1 2-2l8.5 8.6 8.5-8.6a1.4 1.4 0 1 1 2 2L13.9 12l8.6 8.6a1.4 1.4 0 0 1 0 1.9"
+										clip-rule="evenodd"></path>
+								</symbol>
+							</use>
+						</svg>
+					</button>
+				</div>
+				<div class="account-widget-body">
+					<p>
+						<span class="header-strong">Let's go!</span>
+						And don’t worry—if you change your mind, you can link your account at any time.
+					</p>
+					<div class="y-form-field field-with-error ">
+						<label>Give it a nickname</label>
+						<input id="ember146" class="ember-text-field ember-view y-input name-input user-data" autocomplete="nope" autocorrect="off" autocapitalize="words" type="text">
+					</div>
+					<div class="y-form-field field-with-error ">
+						<label>What type of account are you adding?</label>
+						<button class="account-type-select-button" type="button">
+							Select account type...
+							<svg class="ynab-new-icon" width="16" height="16">
+								<!---->
+								<use href="#icon_sprite_caret_right">
+									<symbol xmlns="http://www.w3.org/2000/svg" id="icon_sprite_caret_right" fill="none" viewBox="0 0 24 24">
+										<path fill="currentColor" d="M19.5 13 6.1 23.8c-.9.7-2.1 0-2.1-1V1.3c0-1 1.2-1.7 2-1l13.5 10.6a1.4 1.4 0 0 1 0 2.2"></path>
+									</symbol>
+								</use>
+							</svg>
+						</button>
+						<!---->
+					</div>
+					 <div class="y-form-field field-with-error  currency-input-group">
+						<label>
+							What is your current account balance?
+						</label>
+						<input id="ember147" class="ember-text-field ember-view y-input balance-input user-data" autocomplete="nope" autocorrect="off" spellcheck="false" autocapitalize="none"
+							inputmode="decimal" type="text">
+						<!---->
+					</div>
+					<!---->
+				</div>
+				<div class="account-widget-footer">
+					<button class="ynab-button primary is-large " type="button" disabled="">Next</button>
+				</div>
+			</div> --}}
+			<!---->
+			<div class="account-widget-step account-widget-add-unlinked-account account-widget-step-with-help-box">
+				<div class="account-widget-header">
+					<button aria-label="Back" title="Back" type="button">
+						<svg class="ynab-new-icon icon-back" width="16" height="16">
+							<!---->
+							<use href="#icon_sprite_chevron_left">
+								<symbol xmlns="http://www.w3.org/2000/svg" id="icon_sprite_chevron_left" fill="none" viewBox="0 0 24 24">
+									<path fill="currentColor" fill-rule="evenodd"
+										d="M18.7 23.6a2 2 0 0 1-2.1 0L5.3 13a1.4 1.4 0 0 1 0-2L16.6.4a2 2 0 0 1 2 0c.7.6.7 1.5 0 2L8.6 12l10.2 9.6c.6.5.6 1.4 0 2" clip-rule="evenodd"></path>
+								</symbol>
+							</use>
+						</svg>
+					</button>
+					<div class="account-widget-header-title">
+						<h1>Select Account Type</h1>
+						<!---->
+					</div>
+					<button aria-label="Close" title="Close" type="button">
+						<svg class="ynab-new-icon icon-close" width="16" height="16">
+							<!---->
+							<use href="#icon_sprite_close">
+								<symbol xmlns="http://www.w3.org/2000/svg" id="icon_sprite_close" fill="none" viewBox="0 0 24 24">
+									<path fill="currentColor" fill-rule="evenodd"
+										d="M22.5 22.5a1.4 1.4 0 0 1-2 0L12 13.9l-8.6 8.6a1.4 1.4 0 0 1-1.9-2l8.6-8.5-8.6-8.5a1.4 1.4 0 0 1 2-2l8.5 8.6 8.5-8.6a1.4 1.4 0 1 1 2 2L13.9 12l8.6 8.6a1.4 1.4 0 0 1 0 1.9"
+										clip-rule="evenodd"></path>
+								</symbol>
+							</use>
+						</svg>
+					</button>
+				</div>
+				<div class="account-widget-body">
+					<div class="account-type-select">
+						<div class="account-type-select-group">
+							<h3>Budget Accounts</h3>
+							<p>Accounts that you'll spend from in the near future (usually within the next year or two).</p>
+							<button class="account-widget-list-button" data-account-type="Checking" type="button">
+								Checking
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="Savings" type="button">
+								Savings
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="Cash" type="button">
+								Cash
+								<!---->
+								<svg class="ynab-new-icon" width="16" height="16">
+									<!---->
+									<use href="#icon_sprite_check">
+										<symbol xmlns="http://www.w3.org/2000/svg" id="icon_sprite_check" fill="none" viewBox="0 0 24 24">
+											<path fill="currentColor" d="m7.4 17.6-5-5a1.4 1.4 0 0 0-2 0 1.4 1.4 0 0 0 0 2l6 6a1.4 1.4 0 0 0 2 0L23.6 5.4a1.4 1.4 0 0 0 0-2 1.4 1.4 0 0 0-2 0z"></path>
+										</symbol>
+									</use>
+								</svg>
+							</button>
+							<button class="account-widget-list-button" data-account-type="CreditCard" type="button">
+								Credit Card
+								<!----->
+							</button>
+							<button class="account-widget-list-button" data-account-type="LineOfCredit" type="button">
+								Line of Credit
+								<!---->
+							</button>
+						</div>
+						<div class="account-type-select-group">
+							<h3>Mortgages and Loans</h3>
+							<p>Accounts that have an outstanding balance you're currently paying off, and aren't spending from.</p>
+							<button class="account-widget-list-button" data-account-type="Mortgage" type="button">
+								Mortgage
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="AutoLoan" type="button">
+								Auto Loan
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="StudentLoan" type="button">
+								Student Loan
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="PersonalLoan" type="button">
+								Personal Loan
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="MedicalDebt" type="button">
+								Medical Debt
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="OtherDebt" type="button">
+								Other Debt
+								<!---->
+							</button>
+						</div>
+						<div class="account-type-select-group">
+							<h3>Tracking Accounts</h3>
+							<p>Accounts that hold money you don't plan to spend soon, such as investments or loans.</p>
+							<button class="account-widget-list-button" data-account-type="OtherAsset" type="button">
+								Asset (e.g. Investment)
+								<!---->
+							</button>
+							<button class="account-widget-list-button" data-account-type="OtherLiability" type="button">
+								Liability
+								<!---->
+							</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<!---->
@@ -98,10 +275,14 @@
 
 @push('scripts')
 	<script>
+		//Autofocus
+		/*document.addEventListener("DOMContentLoaded", function() {
+			document.getElementById("ember146").focus();
+		});*/
 		//Cierra el modal
 		document.addEventListener('DOMContentLoaded', function () {
 			const closeButton = document.querySelector('button[aria-label="Close"]');
-			
+
 			closeButton.addEventListener('click', function () {
 				const modalActive = document.getElementById('ember145');
 				modalActive.classList.remove('modal-overlay', 'active');
